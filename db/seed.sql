563353640f0ae149fe5376de2ea384f7995c8792
phamdat <phamdat.ltd@gmail.com>
2020-11-13 11:17:46 +0700

    create manager table

--- db/seed.sql
+++ db/seed.sql
@@ -10,11 +10,11 @@ insert into categories(name) values('Trái cây');
 insert into categories(name) values('Rau củ');
 
 insert into products(user_id, category_id, name, description, img_link, quantity, unit_type, price)
-  values(1, 1, 'Táo', 'Vẫn còn trên cây','db/seed/images/product1.png', 10, 'Kg', 100000),
+  values(1, 1, 'Táo', 'Vẫn còn trên cây','db/images/seed/product1.png', 10, 'Kg', 100000),
         (1, 1, 'Dưa hấu', 'Vẫn còn trên cây dưa','db/images/seed/product2.jpg', 20, 'Quả', 80000),
-        (1, 1, 'Xoài', 'Chua lắm','db/seed/images/product3.jpg', 50, 'Kg', 40000),
-        (1, 1, 'Mít', 'Mít !!!!','db/seed/images/product4.png', 10, 'Quả', 30000),
-        (1, 1, 'Khế', 'Khế chua','db/seed/images/product5.jpg', 100, 'Quả', 5000);
+        (1, 1, 'Xoài', 'Chua lắm','db/images/seed/product3.jpg', 50, 'Kg', 40000),
+        (1, 1, 'Mít', 'Mít !!!!','db/images/seed/product4.png', 10, 'Quả', 30000),
+        (1, 1, 'Khế', 'Khế chua','db/images/seed/product5.jpg', 100, 'Quả', 5000);
 insert into products(user_id, category_id, name, description, img_link, quantity, unit_type, price)
   values(1, 2, 'Cà chua', 'Màu đỏ','db/images/seed/product6.jpeg', 80, 'Kg', 15000),
         (1, 2, 'Khoai lang', 'Khoai lang vàng','db/images/seed/product7.jpg', 20, 'Củ', 80000),
